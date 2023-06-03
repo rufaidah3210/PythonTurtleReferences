35f6dceea335d5cf3590083f3a84e2b88376b9c1
rufaidah3210 <rufaidah3210@gmail.com>
2023-06-03 14:21:08 +0600

    sdf

---
+++ turtle/example1/example_1.2.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.speed(1)
+turtle.width(1)
+
+turtle.fd(100)
+turtle.left(90)
+turtle.fd(100)
+turtle.lt(90)
+turtle.fd(100)
+turtle.lt(90)
+turtle.fd(100)
+turtle.done()
---
+++ turtle/example2/example._2.1.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.forward(90)
+
+turtle.left(90)
+turtle.forward(90)
+
+turtle.right(90)
+turtle.forward(90)
+
+turtle.left(90)
+turtle.forward(90)
+
+turtle.right(90)
+turtle.forward(90)
+
+turtle.left(90)
+turtle.forward(90)
+
+turtle.done()
